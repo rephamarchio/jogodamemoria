383f8f51a56082656d94013d618a12fd92496e8c
rafael miranda <rnet.donwload.com@gmail.com>
2020-04-17 17:48:43 -0300

    #scripit de animação das cartas

--- js/script.js
+++ js/script.js
@@ -9,6 +9,15 @@
 				 card.style.left = i % 8 === 0 ? 5 +"px" : i % 8 * 165 +5 +"px";
 				 //usando ternario para formataçao vertical
 				 card.style.top = i < 8 ? 5 +"px": 250 +"px";
+				
+				 card.addEventListener("click",flipCard,false);
+
 				}}
+
+				function flipCard() {
+					var faces = this.getElementsByClassName('face');
+					faces[0].classList.toggle("flipped");
+					faces[1].classList.toggle("flipped");
+				}
 	
 }());
